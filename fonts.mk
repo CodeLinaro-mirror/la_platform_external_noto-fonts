b67e1985e7581ba5bb180d582bc50902115f1f6c
Pranjal Nayak <pranjal.nayak@reverieinc.com>
2015-04-01 15:22:20 +0530

    noto-fonts: Added more reverie fonts
    (cherry picked from commit df68967619a9f5aace8017c433330fbf50bc9115)
    
    CRs-Fixed: 785191
    
    Change-Id: Id9745f87cfb8bfc9fda93a2ea854b9b9e02a1bb7

--- fonts.mk
+++ fonts.mk
@@ -104,17 +104,28 @@ PRODUCT_PACKAGES := \
     NotoNaskhUI-Bold.ttf \
     NotoSansArmenian-Regular.ttf \
     NotoSansArmenian-Bold.ttf \
+    NotoSansBalinese-Regular.ttf \
+    NotoSansBatak-Regular.ttf \
+    NotoSansBuginese-Regular.ttf \
+    NotoSansBuhid-Regular.ttf \
     NotoSansCanadianAboriginal-Regular.ttf \
+    NotoSansCham-Regular.ttf \
+    NotoSansCham-Bold.ttf \
     NotoSansCherokee-Regular.ttf \
+    NotoSansCoptic-Regular.ttf \
     NotoSansEthiopic-Regular.ttf \
     NotoSansEthiopic-Bold.ttf \
     NotoSansGeorgian-Regular.ttf \
     NotoSansGeorgian-Bold.ttf \
+    NotoSansGlagolitic-Regular.ttf \
     NotoSansHans-Regular.otf \
     NotoSansHant-Regular.otf \
+    NotoSansHanunoo-Regular.ttf \
     NotoSansHebrew-Regular.ttf \
     NotoSansHebrew-Bold.ttf \
+    NotoSansJavanese-Regular.ttf \
     NotoSansJP-Regular.otf \
+    NotoSansKayahLi-Regular.ttf \
     NotoSansKhmer-Regular.ttf \
     NotoSansKhmer-Bold.ttf \
     NotoSansKhmerUI-Regular.ttf \
@@ -124,17 +135,32 @@ PRODUCT_PACKAGES := \
     NotoSansLao-Bold.ttf \
     NotoSansLaoUI-Regular.ttf \
     NotoSansLaoUI-Bold.ttf \
+    NotoSansLepcha-Regular.ttf \
+    NotoSansLimbu-Regular.ttf \
     NotoSansMalayalam-Regular.ttf \
     NotoSansMalayalam-Bold.ttf \
     NotoSansMalayalamUI-Regular.ttf \
     NotoSansMalayalamUI-Bold.ttf \
+    NotoSansMeeteiMayek-Regular.ttf \
+    NotoSansOlChiki-Regular.ttf \
+    NotoSansRejang-Regular.ttf \
+    NotoSansSaurashtra-Regular.ttf \
     NotoSansSinhala-Regular.ttf \
     NotoSansSinhala-Bold.ttf \
+    NotoSansSundanese-Regular.ttf \
+    NotoSansSylotiNagri-Regular.ttf \
     NotoSansSymbols-Regular-Subsetted.ttf \
+    NotoSansTagbanwa-Regular.ttf \
+    NotoSansTaiLe-Regular.ttf \
+    NotoSansTaiTham-Regular.ttf \
+    NotoSansTaiViet-Regular.ttf \
+    NotoSansThaana-Regular.ttf \
+    NotoSansThaana-Bold.ttf \
     NotoSansThai-Regular.ttf \
     NotoSansThai-Bold.ttf \
     NotoSansThaiUI-Regular.ttf \
     NotoSansThaiUI-Bold.ttf \
+    NotoSansTifinagh-Regular.ttf \
     NotoSansYi-Regular.ttf \
     NotoSerif-Regular.ttf \
     NotoSerif-Bold.ttf \
